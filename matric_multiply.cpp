51834f069c255bd78011d72a016d7a25f43009dc
Muhammad Sarim <36129452+muhammadsarim555@users.noreply.github.com>
2021-11-17 14:01:47 +0500

    Fix Matrix Multiply issue

---
+++ matric_multiply.cpp
@@ -0,0 +1,95 @@
+	#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	int a[5][5];
+	int b[5][5];
+	int rslt[5][5];
+	
+	int row, column, row2, column2;
+	
+	cout << "Enter rows matrix 1 = ";
+	cin >> row;
+	
+	cout << "Enter Columns matrix 1 = ";
+	cin >> column;
+
+
+		
+
+			
+		
+		
+	cout << "First Matrix Number";
+	for(int i = 0 ; i < row; i++)
+	for(int j = 0 ; j < column; j++)
+{
+	cout << endl;
+	cin >> a[i][j];	
+}
+
+
+	for(int i = 0 ; i < row; i++)
+{
+	for(int j = 0 ; j < column; j++)
+	
+	cout << a[i][j] << " ";
+	cout << endl;
+}
+
+	cout << "Enter rows matrix 2 = ";
+	cin >> row2;
+	
+	cout << "Enter Columns matrix 2 = ";
+	cin >> column2;
+
+
+
+	cout << "Enter Second Matrix Number";
+	for(int h = 0; h< row2; h++) 
+	for(int k = 0 ; k < column2; k++)
+{
+	cout << endl;
+	cin >> b[h][k];
+}
+	
+	
+	
+	for(int i = 0 ; i < row2; i++)
+{
+	for(int j = 0 ; j < column2; j++)
+	cout << b[i][j] << " ";
+	cout << endl;
+}
+	
+	
+		if(column != row2) 
+{
+	cout << "Matrix Rule Break";
+	exit(1);
+}
+	
+	
+	cout << "\n";
+	cout << "Multiplication of matrix";
+	cout << "\n";
+		
+	for(int i=0;i<row;i++)    
+  
+	for(int j=0;j<column2;j++)    
+{    
+	rslt[i][j]=0;    
+	for(int k=0;k<column ;k++)    
+	rslt[i][j]+=a[i][k]*b[k][j];    
+}    
+
+for(int i=0;i<row;i++)
+{
+	   
+for(int j=0;j< column2;j++)    
+	cout << rslt[i][j] << " ";
+	cout << endl;
+}
+}
